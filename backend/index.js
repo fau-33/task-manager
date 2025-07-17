1188bd6926ac6632df6d4ab0b13f9e192677071d
Flávio Félix <fau_felix30@hotmail.com>
2025-07-17 19:23:56 -0300

    feat: add route to get a task by id

--- backend/index.js
+++ backend/index.js
@@ -20,6 +20,21 @@ app.get("/tasks", async (req, res) => {
   }
 });
 
+app.get("/tasks/:id", async (req, res) => {
+  try {
+    const taskId = req.params.id;
+    const task = await TaskModel.findById(taskId);
+
+    if (!task) {
+      return res.status(500).send("Task not found");
+    }
+
+    return res.status(200).send(task);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
 app.post("/tasks", async (req, res) => {
   try {
     const newTask = new TaskModel(req.body);
@@ -36,7 +51,7 @@ app.delete("/tasks/:id", async (req, res) => {
     const taskToDelete = await TaskModel.findById(taskId);
 
     if (!taskToDelete) {
-      return res.status(500).send("Task not found");
+      return res.status(404).send("Task not found");
     }
 
     const deletedTask = await TaskModel.findByIdAndDelete(taskId);
